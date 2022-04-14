807158ead8a7017d73e93f7232c2195956b87511
Daniel Namir <ddnnll@email.arizona.edu>
2022-04-14 15:50:50 -0700

    Initial commit + welcome page done

---
+++ app/src/main/java/com/example/spotifyclone/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.spotifyclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startExploring = (Button) findViewById(R.id.startExploringButton);
+        startExploring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHomePage();
+            }
+        });
+    }
+
+    public void openHomePage() {
+        Intent homePageIntent = new Intent(this, HomePageActivity.class);
+        startActivity(homePageIntent);
+    }
+}
